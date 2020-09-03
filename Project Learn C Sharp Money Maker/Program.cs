be63f7e623401be35a47666af25e45d7404729ba
Neely_Tristian <Neely_Tristian@edu.mcctc.net>
2020-09-03 12:19:51 -0400

    Add project files.

---
+++ Project Learn C Sharp Money Maker/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Project_Learn_C_Sharp_Money_Maker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Intro
+            Console.WriteLine("Welcome to Money Maker!");
+
+            // 2
+            Console.WriteLine("Enter an amount to convert to coins: ");
+            string convert = Console.ReadLine();
+
+            // 3
+
+               double convert2 = Convert.ToDouble(convert);
+               Console.WriteLine($"{convert}  cents is equal to...");
+
+            // 6
+
+            int goldValue = 10;
+            int silverValue = 5;
+
+            // 7
+            double goldCoins = Math.Floor(convert2 / 10);
+
+            // 8
+            double leftOver = convert2 % 10;
+
+            // 9
+            Double silverCoins = Math.Floor(leftOver / silverValue);
+
+            // 10
+            leftOver = leftOver % silverValue;
+
+            // 11
+
+            Console.WriteLine($"Gold Coins {goldCoins} Silver Coins {silverCoins}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
